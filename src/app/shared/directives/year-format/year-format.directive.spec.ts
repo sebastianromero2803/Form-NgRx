e3bac70b0ac5edd638a1537f1a37982f9f8d193a
sebastianromero2803 <sebasromero2803@gmail.com>
2022-06-20 00:57:38 -0500

    Full

--- src/app/shared/directives/year-format/year-format.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { YearFormatDirective } from './year-format.directive';
-
-describe('YearFormatDirective', () => {
-  it('should create an instance', () => {
-    const directive = new YearFormatDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/shared/pipes/modify-text/modify-text.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ModifyTextPipe } from './modify-text.pipe';
-
-describe('ModifyTextPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ModifyTextPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
